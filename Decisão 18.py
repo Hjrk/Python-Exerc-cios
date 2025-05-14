eaeb94a26039bb9c3e2174070f72c7967f26fd06
Hjrk <119093015+Hjrk@users.noreply.github.com>
2025-05-14 15:05:08 -0300

    Atualizar o Decisão 18.py

--- Decisão 18.py
+++ Decisão 18.py
@@ -1,15 +1,2 @@
-#Exercicio Nr. 18
-from datetime import date
-x = []
-while len(x) == 0:
- a = input('digite a data de seu nascimento: dd/mm/aaaa')
- b = a.split('/', 2)
- if len (b) == 3:
-   if bool (date(year=int(b[2]), month=int(b[1]), day=int(b[0]))) :
-     print ('data válida')
-     x.append(1)
-   else:
-     print ('valor inesperado! tente novamente')
- else:
-   print ('valor inesperado! tente novamente!')
+
 
